9ef8d33073b8e7d6edb75141cafd3c95eb7b8959
김예린 <yerin.3321@daum.net>
2024-09-12 19:45:37 +0900

    충돌해결3

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="callback" index element={<HomePage />} />
         <Route path="search" element={<SearchPage />} />{" "}
-        {/* /search 경로에서 SearchPage 렌더링 */}
+        {/* /search 경로에서 SearchPage 렌더링1 */}
         <Route path="detail/:type/:id" element={<DetailPage />} />
         <Route path="detail/ArtistInfoPage" element={<ArtistInfoPage />} />
       </Route>
