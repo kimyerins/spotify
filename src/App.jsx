4b06fd48283d1be58c74b48c4311654dfef89edd
JiHun Kim <dydekffrl@naver.com>
2024-09-13 03:47:55 +0900

    add : [앨범페이지] ControllerBar추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
-import SearchPage from "./pages/SearchPage/SearchPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import DetailPage from "./pages/DetailPage/DetailPage";
-import AlbumPage from "./pages/AlbumPage/AlbumPage";
 import Layout from "./layout/layout";
 import ArtistInfoPage from "./pages/DetailPage/ArtistInfoPage";
 import DetailPage from "./pages/DetailPage/DetailPage";
 import HomePage from "./pages/HomePage/HomePage";
 import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 import SearchPage from "./pages/SearchPage/SearchPage";
+import AlbumPage from "./pages/AlbumPage/AlbumPage";
 
 function App() {
   return (
@@ -22,7 +17,7 @@ function App() {
           <Route path="search" index element={<SearchPage />} />
           <Route path="detail/:type/:id" element={<DetailPage />} />
           <Route path="detail/ArtistInfoPage" element={<ArtistInfoPage />} />
-          <Route path="detail/album" element={<AlbumPage />} />
+          <Route path="detail/AlbumPage" element={<AlbumPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
--- src/pages/AlbumPage/AlbumPage.jsx
+++ src/pages/AlbumPage/AlbumPage.jsx
@@ -1,21 +1,46 @@
 import React from "react";
 import AlbumBanner from "./components/AlbumBanner";
 import Card from "../../common/Card";
+import { Link } from "react-router-dom";
+import ControllerBar from "./components/ControllerBar";
+import TrackList from "./components/TrackList";
 
 const AlbumPage = () => {
   return (
-    <div>
+    <div
+      className={`w-[100%]  bg-cover self-stretch h-[100%]   rounded-md bg-[#283423] bg-gradient-to-b from-transparent to-black`} //배경색상은 동적으로 변경할 예정.
+      // style={{
+      //   backgroundImage: `url(
+      // "https://i.namu.wiki/i/3B-OpZ4Zv3EHLm1L1u0vOWjT2Sy4uAT43W93T0QzZW-YhxaP8ECybTqzArW3u6xA86NG-GOWWPPnNyUgPMzllQ.webp")`,
+      //   filter: "blur(20px)",
+      // }}
+    >
+      {/* 앨범 배너 */}
       <div>
-        <div
-          className="w-full absolute  bg-cover self-stretch h-[100%]  rounded-md bg-[#283423]" //배경색상은 동적으로 변경할 예정.
-          // style={{
-          //   backgroundImage: `url(
-          // "https://i.namu.wiki/i/3B-OpZ4Zv3EHLm1L1u0vOWjT2Sy4uAT43W93T0QzZW-YhxaP8ECybTqzArW3u6xA86NG-GOWWPPnNyUgPMzllQ.webp")`,
-          //   filter: "blur(20px)",
-          // }}
-        ></div>
         <AlbumBanner />
-        <Card title="정국" subTitle="아티스트" />
+      </div>
+      {/* 트랙리스트 섹션 */}
+      <div className="p-4">
+        <ControllerBar />
+        <TrackList />
+      </div>
+      <div>
+        <div className="flex justify-between items-center text-white p-4">
+          <Link
+            to={"/search"}
+            className="text-2xl font-bold cursor-pointer hover:underline"
+          >
+            xx님의 곡 더보기
+          </Link>
+          <div>디스코 그래피보기</div>
+        </div>
+        <div className="flex justify-around">
+          <Card title="정국" subTitle="아티스트" />
+          <Card title="정국" subTitle="아티스트" />
+          <Card title="정국" subTitle="아티스트" />
+          <Card title="정국" subTitle="아티스트" />
+          <Card title="정국" subTitle="아티스트" />
+        </div>
       </div>
     </div>
   );
