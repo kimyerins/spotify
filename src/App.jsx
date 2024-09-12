e7f25fda5cec60dcc1b0012e36c81325232d3378
김예린 <yerin.3321@daum.net>
2024-09-13 02:21:46 +0900

    app.jsx 충돌해결

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,36 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Layout from "./layout/layout";
 import ArtistInfoPage from "./pages/DetailPage/ArtistInfoPage";
+import DetailPage from "./pages/DetailPage/DetailPage";
+import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import SearchPage from "./pages/SearchPage/SearchPage";
+import { Provider } from "react-redux";
+import store from "./redux/store.jsx";
+import ProtectPage from "./layout/Component/ProtectPage.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        {" "}
-        {/* Layout 컴포넌트는 항상 렌더링 */}
-        <Route index element={<HomePage />} />
-        <Route path="callback" index element={<HomePage />} />
-        <Route path="search" element={<SearchPage />} />{" "}
-        {/* /search 경로에서 SearchPage 렌더링 */}
-        <Route path="detail/:type/:id" element={<DetailPage />} />
-        <Route path="detail/ArtistInfoPage" element={<ArtistInfoPage />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Provider store={store}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <ProtectPage>
+                <Layout />
+              </ProtectPage>
+            }
+          >
+            <Route index element={<HomePage />} />
+            <Route path="search" index element={<SearchPage />} />
+            <Route path="detail/:type/:id" element={<DetailPage />} />
+            <Route path="detail/ArtistInfoPage" element={<ArtistInfoPage />} />
+          </Route>
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Provider>
+    </>
   );
 }
 
