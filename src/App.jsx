1dcfcb72c62eb6ebb2719b0b7c9705b8da626893
Suzy Kim <skme1103@naver.com>
2024-09-12 14:46:50 +0900

    Add info card components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/layout";
-import HomePage from "./pages/HomePage/HomePage";
-import SearchPage from "./pages/SearchPage/SearchPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import ArtistInfoPage from "./pages/DetailPage/ArtistInfoPage";
 import DetailPage from "./pages/DetailPage/DetailPage";
+import HomePage from "./pages/HomePage/HomePage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
+import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
 
@@ -16,6 +16,7 @@ function App() {
           <Route index element={<HomePage />} />
           <Route path="search" index element={<SearchPage />} />
           <Route path="detail/:type/:id" element={<DetailPage />} />
+          <Route path="detail/ArtistInfoPage" element={<ArtistInfoPage />} />
         </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
