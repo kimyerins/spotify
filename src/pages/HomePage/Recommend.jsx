372e79de8767d94549007e46845ac90966d0a702
김예린 <yerin.3321@daum.net>
2024-09-15 08:32:27 +0900

    recommend.jsx 에러반환 수정

--- src/pages/HomePage/Recommend.jsx
+++ src/pages/HomePage/Recommend.jsx
@@ -1,39 +1,36 @@
-import React from 'react';
+import React from "react";
 import Card from "../../common/Card.jsx";
-import {useRecommendTrack} from "../../hooks/useRecomendTracks.js";
+import { useRecommendTrack } from "../../hooks/useRecomendTracks.js";
 
-const Recommend = ({data}) => {
+const Recommend = ({ data }) => {
+  console.log("eeeerrrerererer");
 
-    console.log('eeeerrrerererer')
+  let { data: recommend, isLoading } = useRecommendTrack(data);
 
-    let {data:recommend,isLoading} = useRecommendTrack(data)
+  console.log("tttttt", recommend);
 
-    console.log('tttttt',recommend)
+  if (isLoading || !recommend || Object.keys(recommend).length === 0) {
+    return "";
+  }
 
-    if(isLoading){
-        return '';
-    }
-
-    if(Object.keys(recommend).length===0){
-        return '';
-    }
-
-    return (
-        <div className={'mt-10'}>
-            <h1>
-                {`오늘의 추천장르!! ${data.toUpperCase()}`}
-            </h1>
-            <div className={'grid grid-cols-[repeat(auto-fit,minmax(165px,1fr))] gap-1'}>
-                {recommend && recommend.map((item, index)=>(
-                    <Card
-                        key={index}
-                        title={item.name}
-                        subTitle={item.artists[0]?.name}
-                        imgUrl={item.album.images[item.album.images.length-2]?.url}/>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div className={"mt-10"}>
+      <h1>{`오늘의 추천장르!! ${data.toUpperCase()}`}</h1>
+      <div
+        className={"grid grid-cols-[repeat(auto-fit,minmax(165px,1fr))] gap-1"}
+      >
+        {recommend &&
+          recommend.map((item, index) => (
+            <Card
+              key={index}
+              title={item.name}
+              subTitle={item.artists[0]?.name}
+              imgUrl={item.album.images[item.album.images.length - 2]?.url}
+            />
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Recommend;
