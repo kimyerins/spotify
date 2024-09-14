9812d99526904b1a33db1dfb7bb027f1c8290412
MiHyeon <kkmmhh7478@gmail.com>
2024-09-15 00:51:37 +0900

    #29 상위결과 링크 적용

--- src/pages/SearchResultPage/SearchResultPage.jsx
+++ src/pages/SearchResultPage/SearchResultPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 import { useSearchQuery } from '../../hooks/useSearch';
 import ChartItem from '../../common/ChartItem';
 import Section from './Components/Section';
@@ -93,29 +93,31 @@ const SearchResultPage = () => {
                     <div className='flex gap-4 w-full mt-6 flex-row max-[1300px]:flex-col '>
                         <div className='w-1/2 max-[1300px]:w-full'>
                             <h2 className='text-2xl mb-2 text-white font-bold'>상위 결과</h2>
-                            <div className='h-56 p-5 bg-[#181818] cursor-pointer hover:bg-[#1f1f1f] rounded-[6px] group transition-all duration-300 ease-in-out '>
-                                <img className='overflow-hidden rounded-full w-24 h-24 mb-5' loading="lazy" src={results?.artists[0]?.images[0]?.url} alt="" />
-                                <div className={'relative'}>
-                                    <span className={'block mb-1 text-4xl text-white font-bold'}>{results?.artists[0]?.name}</span>
-                                    <span className={'text-sm text-[#b3b3b3]'}>{categoryTranslations[results?.artists[0]?.type + 's'] || results?.artists[0]?.type}</span>
-                                    <div className={'absolute bottom-2 right-2 transform translate-y-2 opacity-0 transition-all duration-500 ease-out group-hover:translate-y-0 group-hover:opacity-100 z-10'}>
-                                        <span
-                                            className={"bg-[#1ed760] w-12 h-12 flex items-center justify-center rounded-full hover:bg-[#3be477] cursor-pointer"}
-                                            style={{ boxShadow: '0 8px 8px rgba(0, 0, 0, 0.3)' }}
-                                        >
-                                            <svg
-                                                role="img"
-                                                aria-hidden="true"
-                                                viewBox="0 0 24 24"
-                                                className="w-6 h-6 text-[#000000]"
-                                                fill="currentColor"
+                            <Link to={`/detail/artist/${results?.artists[0]?.name}`}>
+                                <div className='h-56 p-5 bg-[#181818] cursor-pointer hover:bg-[#1f1f1f] rounded-[6px] group transition-all duration-300 ease-in-out'>
+                                    <img className='overflow-hidden rounded-full w-24 h-24 mb-5' loading="lazy" src={results?.artists[0]?.images[0]?.url} alt="" />
+                                    <div className={'relative'}>
+                                        <span className={'block mb-1 text-4xl text-white font-bold'}>{results?.artists[0]?.name}</span>
+                                        <span className={'text-sm text-[#b3b3b3]'}>{categoryTranslations[results?.artists[0]?.type + 's'] || results?.artists[0]?.type}</span>
+                                        <div className={'absolute bottom-2 right-2 transform translate-y-2 opacity-0 transition-all duration-500 ease-out group-hover:translate-y-0 group-hover:opacity-100 z-10'}>
+                                            <span
+                                                className={"bg-[#1ed760] w-12 h-12 flex items-center justify-center rounded-full hover:bg-[#3be477] cursor-pointer"}
+                                                style={{ boxShadow: '0 8px 8px rgba(0, 0, 0, 0.3)' }}
                                             >
-                                                <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
-                                            </svg>
-                                        </span>
+                                                <svg
+                                                    role="img"
+                                                    aria-hidden="true"
+                                                    viewBox="0 0 24 24"
+                                                    className="w-6 h-6 text-[#000000]"
+                                                    fill="currentColor"
+                                                >
+                                                    <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
+                                                </svg>
+                                            </span>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
+                            </Link>
                         </div>
                         <div className='w-1/2 max-[1300px]:w-full'>
                             <h2 className='text-2xl mb-2 text-white font-bold'>곡</h2>
