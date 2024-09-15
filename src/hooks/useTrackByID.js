e74da427a6e2bd6b58c4f7265a648a1e2fb2eb39
SeokHoon <sughoon1@gmail.com>
2024-09-15 16:07:04 +0900

    id값으로 트랙 데이터 요청하는 훅 생성

---
+++ src/hooks/useTrackByID.js
@@ -0,0 +1,22 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../utils/useApi.jsx";
+
+
+const fetchTrack = async (ids) => {
+    return api.get(`/tracks/${ids}`);
+};
+
+export const useTrackByID = (ids) => {
+
+
+    return useQuery({
+        queryKey: ["trackById",ids],
+        queryFn: () => fetchTrack(ids),
+        retry: false,
+        select: (result) => result.data,
+        staleTime: 1000 * 60 * 10, // 10분 동안 데이터가 stale되지 않음
+        gcTime: 1000 * 60 * 60, // 60분 동안 캐시에 유지됨
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+    });
+};
